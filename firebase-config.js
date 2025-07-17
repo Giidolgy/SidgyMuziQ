a1afb3114a410cd2580155c108a3858e87c82d0d
Giidolgy <shelsioidolgy@gmail.com>
2025-07-17 17:01:42 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBHY_c2Osu_1z_3_OeypN78q9IJTXLgSIA",
+  authDomain: "sidgymuziq.firebaseapp.com",
+  projectId: "sidgymuziq",
+  storageBucket: "sidgymuziq.appspot.com",
+  messagingSenderId: ""202229787182"",
+  appId: "1:202229787182:web:32e5ffc3aa8830bb384dbf"
+};
+
+firebase.initializeApp(firebaseConfig);
